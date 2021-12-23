93493fc6784f412b9c33872cfca660c3c6ebe38e
Yury Gorbachev <yury.gorbachev@intel.com>
2021-12-23 14:48:13 +0300

    initial version

--- README.md
+++ README.md
@@ -1,129 +1,48 @@
-# RetinaFace in PyTorch
+# RetinaFace in OpenVINO
 
-A [PyTorch](https://pytorch.org/) implementation of [RetinaFace: Single-stage Dense Face Localisation in the Wild](https://arxiv.org/abs/1905.00641). Model size only 1.7M, when Retinaface use mobilenet0.25 as backbone net. We also provide resnet50 as backbone net to get better result. The official code in Mxnet can be found [here](https://github.com/deepinsight/insightface/tree/master/RetinaFace).
+This is OpenVINO based demo for RetinaFace. Forked from PyTorch implementation that is available here: [https://github.com/wang-xinyu/Pytorch_Retinaface](https://github.com/wang-xinyu/Pytorch_Retinaface).
 
-## Mobile or Edge device deploy
-We also provide a set of Face Detector for edge device in [here](https://github.com/biubug6/Face-Detector-1MB-with-landmark) from python training to C++ inference.
+## Steps to reproduce
 
-## WiderFace Val Performance in single scale When using Resnet50 as backbone net.
-| Style | easy | medium | hard |
-|:-|:-:|:-:|:-:|
-| Pytorch (same parameter with Mxnet) | 94.82 % | 93.84% | 89.60% |
-| Pytorch (original image scale) | 95.48% | 94.04% | 84.43% |
-| Mxnet | 94.86% | 93.87% | 88.33% |
-| Mxnet(original image scale) | 94.97% | 93.89% | 82.27% |
-
-## WiderFace Val Performance in single scale When using Mobilenet0.25 as backbone net.
-| Style | easy | medium | hard |
-|:-|:-:|:-:|:-:|
-| Pytorch (same parameter with Mxnet) | 88.67% | 87.09% | 80.99% |
-| Pytorch (original image scale) | 90.70% | 88.16% | 73.82% |
-| Mxnet | 88.72% | 86.97% | 79.19% |
-| Mxnet(original image scale) | 89.58% | 87.11% | 69.12% |
-<p align="center"><img src="curve/Widerface.jpg" width="640"\></p>
-
-## FDDB Performance.
-| FDDB(pytorch) | performance |
-|:-|:-:|
-| Mobilenet0.25 | 98.64% |
-| Resnet50 | 99.22% |
-<p align="center"><img src="curve/FDDB.png" width="640"\></p>
-
-### Contents
-- [Installation](#installation)
-- [Training](#training)
-- [Evaluation](#evaluation)
-- [TensorRT](#tensorrt)
-- [References](#references)
-
-## Installation
-##### Clone and install
-1. git clone https://github.com/biubug6/Pytorch_Retinaface.git
-
-2. Pytorch version 1.1.0+ and torchvision 0.3.0+ are needed.
-
-3. Codes are based on Python 3
-
-##### Data
-1. Download the [WIDERFACE](http://shuoyang1213.me/WIDERFACE/WiderFace_Results.html) dataset.
-
-2. Download annotations (face bounding boxes & five facial landmarks) from [baidu cloud](https://pan.baidu.com/s/1Laby0EctfuJGgGMgRRgykA) or [dropbox](https://www.dropbox.com/s/7j70r3eeepe4r2g/retinaface_gt_v1.1.zip?dl=0)
-
-3. Organise the dataset directory as follows:
-
+### Clone source and install packages
 ```Shell
-  ./data/widerface/
-    train/
-      images/
-      label.txt
-    val/
-      images/
-      wider_val.txt
+git clone https://github.com/yury-gorbachev/OpenVINO_Retinaface
+conda create --name ov_retinaface python=3.7
+conda activate ov_retinaface
+cd OpenVINO_Retinaface
+pip install -r requirements.txt
 ```
-ps: wider_val.txt only include val file names but not label information.
 
-##### Data1
-We also provide the organized dataset we used as in the above directory structure.
+### Install OpenVINO and development tools
+```Shell
+pip install openvino-dev
+```
 
-Link: from [google cloud](https://drive.google.com/open?id=11UGV3nbVv1x9IC--_tK3Uxf7hA6rlbsS) or [baidu cloud](https://pan.baidu.com/s/1jIp9t30oYivrAvrgUgIoLQ) Password: ruck
+### Download pretrained models as per original repo instructions
 
-## Training
-We provide restnet50 and mobilenet0.25 as backbone network to train model.
-We trained Mobilenet0.25 on imagenet dataset and get 46.58%  in top 1. If you do not wish to train the model, we also provide trained model. Pretrain model  and trained model are put in [google cloud](https://drive.google.com/open?id=1oZRSG0ZegbVkVwUd8wUIQx8W7yfZ_ki1) and [baidu cloud](https://pan.baidu.com/s/12h97Fy1RYuqMMIV-RpzdPg) Password: fstq . The model could be put as follows:
+Pretrain model  and trained model are put in [google cloud](https://drive.google.com/open?id=1oZRSG0ZegbVkVwUd8wUIQx8W7yfZ_ki1) and [baidu cloud](https://pan.baidu.com/s/12h97Fy1RYuqMMIV-RpzdPg) Password: fstq . The model could be put as follows:
 ```Shell
   ./weights/
       mobilenet0.25_Final.pth
       mobilenetV1X0.25_pretrain.tar
       Resnet50_Final.pth
 ```
-1. Before training, you can check network configuration (e.g. batch_size, min_sizes and steps etc..) in ``data/config.py and train.py``.
 
-2. Train the model using WIDER FACE:
-  ```Shell
-  CUDA_VISIBLE_DEVICES=0,1,2,3 python train.py --network resnet50 or
-  CUDA_VISIBLE_DEVICES=0 python train.py --network mobile0.25
-  ```
+### Convert model to onnx (Will generate MobileNet baset model by default)
 
-
-## Evaluation
-### Evaluation widerface val
-1. Generate txt file
 ```Shell
-python test_widerface.py --trained_model weight_file --network mobile0.25 or resnet50
+python ./convert_to_onnx.py
 ```
-2. Evaluate txt results. Demo come from [Here](https://github.com/wondervictor/WiderFace-Evaluation)
+This will generate FaceDetector.onnx
+
+### Convert model to OpenVINO IR (Will generate MobileNet baset model by default)
+
 ```Shell
-cd ./widerface_evaluate
-python setup.py build_ext --inplace
-python evaluation.py
+mo --use_new_frontend --mean_values="[104, 117, 123]" --input_model=FaceDetector.onnx --model_name=FaceDetector_MN
 ```
-3. You can also use widerface official Matlab evaluate demo in [Here](http://mmlab.ie.cuhk.edu.hk/projects/WIDERFace/WiderFace_Results.html)
-### Evaluation FDDB
+This will generate FaceDetector_MN.xml and *.bin files that represent model in OV IR.
 
-1. Download the images [FDDB](https://drive.google.com/open?id=17t4WULUDgZgiSy5kpCax4aooyPaz3GQH) to:
+### Run demo (requries camera)
 ```Shell
-./data/FDDB/images/
-```
-
-2. Evaluate the trained model using:
-```Shell
-python test_fddb.py --trained_model weight_file --network mobile0.25 or resnet50
-```
-
-3. Download [eval_tool](https://bitbucket.org/marcopede/face-eval) to evaluate the performance.
-
-<p align="center"><img src="curve/1.jpg" width="640"\></p>
-
-## TensorRT
--[TensorRT](https://github.com/wang-xinyu/tensorrtx/tree/master/retinaface)
-
-## References
-- [FaceBoxes](https://github.com/zisianw/FaceBoxes.PyTorch)
-- [Retinaface (mxnet)](https://github.com/deepinsight/insightface/tree/master/RetinaFace)
-```
-@inproceedings{deng2019retinaface,
-title={RetinaFace: Single-stage Dense Face Localisation in the Wild},
-author={Deng, Jiankang and Guo, Jia and Yuxiang, Zhou and Jinke Yu and Irene Kotsia and Zafeiriou, Stefanos},
-booktitle={arxiv},
-year={2019}
+python openvino_webcam_demo.py
 ```
---
+++ openvino_webcam_demo.py
@@ -0,0 +1,227 @@
+#!/usr/bin/env python
+
+import cv2
+import numpy as np
+import time
+
+from math import ceil
+from itertools import product as product
+
+from openvino.runtime import Core, Layout, Type
+from openvino.preprocess import PrePostProcessor, ResizeAlgorithm
+
+from data import cfg_mnet, cfg_re50
+from utils.nms.py_cpu_nms import py_cpu_nms
+
+#import torch
+
+def compute_priors(cfg, image_size):
+    min_sizes_cfg = cfg['min_sizes']
+    steps = cfg['steps']
+    clip = cfg['clip']
+    feature_maps = [[ceil(image_size[0]/step), ceil(image_size[1]/step)] for step in steps]
+    name = "s"
+    anchors = []
+    for k, f in enumerate(feature_maps):
+        min_sizes = min_sizes_cfg[k]
+        for i, j in product(range(f[0]), range(f[1])):
+            for min_size in min_sizes:
+                s_kx = min_size / image_size[1]
+                s_ky = min_size / image_size[0]
+                dense_cx = [x * steps[k] / image_size[1] for x in [j + 0.5]]
+                dense_cy = [y * steps[k] / image_size[0] for y in [i + 0.5]]
+                for cy, cx in product(dense_cy, dense_cx):
+                        anchors += [cx, cy, s_kx, s_ky]
+
+    # back to torch land
+    output =np.asarray(anchors).reshape(-1,4)
+    if clip:
+        output=np.clip(output, 0, 1)
+    return output
+
+
+def decode_boxes(loc, priors, variances):
+    """Decode locations from predictions using priors to undo
+    the encoding we did for offset regression at train time.
+    Args:
+        loc (tensor): location predictions for loc layers,
+            Shape: [num_priors,4]
+        priors (tensor): Prior boxes in center-offset form.
+            Shape: [num_priors,4].
+        variances: (list[float]) Variances of priorboxes
+    Return:
+        decoded bounding box predictions
+    """
+
+    boxes = np.concatenate((
+        priors[:, :2] + loc[:, :2] * variances[0] * priors[:, 2:], 
+        priors[:, 2:] * np.exp(loc[:, 2:] * variances[1])),
+        axis=1)
+    boxes[:, :2] -= boxes[:, 2:] / 2
+    boxes[:, 2:] += boxes[:, :2]
+
+    return boxes
+
+def decode_landmarks(pre, priors, variances):
+    """Decode landm from predictions using priors to undo
+    the encoding we did for offset regression at train time.
+    Args:
+        pre (tensor): landm predictions for loc layers,
+            Shape: [num_priors,10]
+        priors (tensor): Prior boxes in center-offset form.
+            Shape: [num_priors,4].
+        variances: (list[float]) Variances of priorboxes
+    Return:
+        decoded landm predictions
+    """
+    landms = np.concatenate((priors[:, :2] + pre[:, :2] * variances[0] * priors[:, 2:],
+                        priors[:, :2] + pre[:, 2:4] * variances[0] * priors[:, 2:],
+                        priors[:, :2] + pre[:, 4:6] * variances[0] * priors[:, 2:],
+                        priors[:, :2] + pre[:, 6:8] * variances[0] * priors[:, 2:],
+                        priors[:, :2] + pre[:, 8:10] * variances[0] * priors[:, 2:],
+                        ), axis=1)
+    return landms
+
+def process_frame(model, input):
+    input=np.expand_dims(input, 0) #HWC->NHWC
+    tic=time.time()
+    output=model.infer_new_request({0: input})
+    output=list(output.values())
+    infer_time=(time.time() - tic)
+    output=(output[0], output[1], output[2])
+    return output, infer_time
+
+def postprocess_frame(cfg, output):
+
+    confidence=output[1]
+    scores = confidence.squeeze(0)[:, 1]
+    locations=output[0].squeeze(0)
+    landmarks=output[2].squeeze(0)
+
+    # ignore low scores
+    indexes = np.where(scores > confidence_threshold)[0]
+    filtered_priors=priors[indexes]
+    filtered_locations=locations[indexes]
+    filtered_landmarks = landmarks[indexes]
+    scores = scores[indexes]
+
+    boxes = decode_boxes(filtered_locations, filtered_priors, cfg['variance'])
+    #to original image size
+    boxes = boxes * np.float32([input_w, input_h, input_w, input_h]) 
+
+    landms = decode_landmarks(filtered_landmarks, filtered_priors, cfg['variance'])
+    #to original image size
+    landms = landms * np.float32([input_w, input_h, input_w, input_h,
+                            input_w, input_h, input_w, input_h,
+                            input_w, input_h])
+
+    # keep top-K before NMS
+    order = scores.argsort()[::-1][:top_k]
+    boxes = boxes[order.copy()]
+    landms = landms[order.copy()]
+    scores = scores[order.copy()]
+
+    # do NMS
+    dets = np.hstack((boxes, scores[:, np.newaxis])).astype(np.float32, copy=False)
+    keep = py_cpu_nms(dets, nms_threshold)
+    # keep = nms(dets, args.nms_threshold,force_cpu=args.cpu)
+    dets = dets[keep, :]
+    landms = landms[keep]
+
+    # keep top-K faster NMS
+    dets = dets[:keep_top_k, :]
+    landms = landms[:keep_top_k, :]
+
+    dets = np.concatenate((dets, landms), axis=1)
+    return dets
+
+#algo settings
+
+#for mobilenet
+model_cfg=cfg_mnet
+modelPath="FaceDetector_MN.xml"
+
+#for resnet-50
+#modelPath="FaceDetector_RN50.xml"
+#model_cfg=cfg_re50
+
+confidence_threshold=0.02
+top_k=int(5)
+nms_threshold=0.4
+keep_top_k=750
+vis_threshold=0.6
+
+#figure out actual size of image that we will be getting from camera
+camera = cv2.VideoCapture(0)
+ret, frame=camera.read()
+input_h, input_w, _ = frame.shape
+
+#create core object
+core=Core()
+#load IR model, converted from ONNX and mean values integrated in model
+model=core.read_model(modelPath)
+
+#this is to learn which input size model expects
+_, _, model_h, model_w = model.inputs[0].get_shape()
+scale_h=input_h/model_h
+scale_w=input_w/model_w
+priors = compute_priors(model_cfg, (model_h, model_w))
+
+
+#preprocessing that will be integrated into compiled model
+#executed for every inference call on target device
+pp=PrePostProcessor(model)
+#input tensor corresponds to what we have got from camera
+pp.input().tensor() \
+    .set_element_type(Type.u8) \
+    .set_layout(Layout('NHWC')) \
+    .set_spatial_static_shape(input_h, input_w)
+#convert input tensor from u8->fp32
+pp.input().preprocess().convert_element_type(Type.f32)
+#resize to model input size
+pp.input().preprocess().resize(ResizeAlgorithm.RESIZE_LINEAR)
+#this will convert layout from NHWC to NCHW 
+pp.input().model().set_layout(Layout('NCHW'))
+#integrate preprocessing into model
+model=pp.build()
+
+#compile model for CPU
+compiledModel=core.compile_model(model, "CPU")
+
+while True:
+    ret, frame = camera.read()
+    
+    if not ret:
+        print("capture failure")
+        break
+        
+    #just pass frame unchanged - preprocessing will happen inside infer call
+    output, infer_time=process_frame(compiledModel, frame)
+    dets=postprocess_frame(model_cfg, output)
+    infer_time='OpenVINO Inference time: '+'{:.0f}ms'.format(infer_time*1000)+' ({:.1f} FPS)'.format(1/infer_time)
+ 
+    #draw detections and landmarks
+    for b in dets:
+        if (b[4]<vis_threshold):
+            continue
+        b = list(map(int, b))
+        cv2.rectangle(frame, (int(b[0]), int(b[1])), (int(b[2]),int(b[3])), (0, 0, 255), 1)
+        
+        cv2.circle(frame, (int(b[5]), int(b[6])), 1, (0, 0, 255), 4)
+        cv2.circle(frame, (int(b[7]), int(b[8])), 1, (0, 255, 255), 4)
+        cv2.circle(frame, (int(b[9]), int(b[10])), 1, (255, 0, 255), 4)
+        cv2.circle(frame, (int(b[11]), int(b[12])), 1, (0, 255, 0), 4)
+        cv2.circle(frame, (int(b[13]), int(b[14])), 1, (255, 0, 0), 4)
+
+    cv2.putText(frame, infer_time, (0, 20),
+                            cv2.FONT_HERSHEY_DUPLEX, 0.5, (255, 255, 255))
+
+    cv2.imshow("output", frame)
+
+    key = cv2.waitKey(1)
+    if key%256==27:
+        break
+
+camera.release()
+
+cv2.destroyAllWindows()
